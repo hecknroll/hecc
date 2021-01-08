a9a9cc965283f71a9455e9436d50797ddb3905b3
Rachel Gina <rgabelarde@gmail.com>
2021-01-08 16:04:05 +0800

    help

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const { listenerCount } = require("node-telegram-bot-api");
+
 require("dotenv").config();
 const BotToken = process.env.BOT_TOKEN;
 var TelegramBot = require("node-telegram-bot-api"),
   telegram = new TelegramBot(BotToken, { polling: true });
 //declare user object
+
 function telegram_user() {
   this.telegram_id = 0; //integer for storing the id
   this.telegram_username = null; //null for storing some string
@@ -15,6 +18,8 @@ function telegram_user() {
   this.store_some_message = null;
 }
 //initialise global user array
+let chatTasklistMap = new Map();
+let tasklist = [];
 var user_array = [];
 user_array.push(new telegram_user()); //i just like to fill the [0] with a blank user
 //scan array for user
@@ -26,56 +31,94 @@ function auth_usr(telegram_id) {
   }
   return 0;
 }
+function newStress(telegramChatId, newTask) {
+  //   if (chatTasklistMap.has(telegramChatId)) {
+    tasklist.push(newTask);
+    chatTasklistMap.set(telegramChatId, { tasklist: tasklist });
+}
 //declare bot logic, on text event
 telegram.on("text", (message) => {
   //instantiate new user state object
-  if (auth_usr(message.from.id) === 0) {
-    user_array.push(new telegram_user());
-    user_array[user_array.length - 1].telegram_id = message.from.id;
-    user_array[user_array.length - 1].telegram_username = message.from.username;
-    user_array[user_array.length - 1].telegram_firstname =
-      message.from.first_name;
-    user_array[user_array.length - 1].telegram_lastname =
-      message.from.last_name;
-    telegram.sendMessage(message.from.id, "Welcome Message!", {
-      parse_mode: "Markdown",
-    });
-    return;
-  }
+  //   if (auth_usr(message.from.id) === 0) {
+  //     user_array.push(new telegram_user());
+  //     user_array[user_array.length - 1].telegram_id = message.from.id;
+  //     user_array[user_array.length - 1].telegram_username = message.from.username;
+  //     user_array[user_array.length - 1].telegram_firstname =
+  //       message.from.first_name;
+  //     user_array[user_array.length - 1].telegram_lastname =
+  //       message.from.last_name;
+  //     telegram.sendMessage(message.from.id, "Welcome Message!", {
+  //       parse_mode: "Markdown",
+  //     });
+  //     return;
+  //   }
   //listen for a bot command
-  if (message.text.toLowerCase().indexOf("/some_command") === 0) {
+  if (message.text.toLowerCase().indexOf("/start") === 0) {
     //then change some state variable
-    user_array[auth_usr(message.from.id)].some_state = 1;
+    // user_array[auth_usr(message.chat.id)].some_state = 1;
+    var user_text = message.text.replace("/start ", "");
+    if (user_text === "/start") {
+      telegram.sendMessage(
+        message.chat.id,
+        "Hello, I see " +
+          message.from.first_name +
+          " requires some peer pressure to keep up with your work. Welcome! Everyone shall be pressured together!",
+        {
+          parse_mode: "Markdown",
+        }
+      );
+    }
     //do some other weird stuff
     return;
   }
   //listen for some other bot command
-  if (message.text.toLowerCase().indexOf("/some_other_command") === 0) {
+  if (message.text.toLowerCase().indexOf("/hello") === 0) {
+    var user_text = message.text.replace("/hello ", "");
+    if (user_text === "/hello") {
+      //then reply to the user with a message
+      telegram.sendMessage(message.chat.id, "Hello " + message.from.first_name);
+      //do some other weird stuff
+    }
+    return;
+  }
+
+  //listen for some other bot command
+  if (message.text.toLowerCase().indexOf("/pmme") === 0) {
     //then reply to the user with a message
-    telegram.sendMessage(message.from.id, "you sent me some_other_command", {
-      parse_mode: "Markdown",
-    });
+    telegram.sendMessage(
+      message.from.id,
+      "Hello, " + message.from.first_name + " you sneaky bastard."
+    );
     //do some other weird stuff
     return;
   }
-  //listen for a command with some text at the end of it
-  if (message.text.toLowerCase().indexOf("/text_input") === 0) {
+//listen for a command with some text at the end of it
+if (message.text.toLowerCase().indexOf("/addstress") === 0) {
     //strip the command from the string, to get the user_text
-    var user_text = message.text.replace("/text_input ", "");
-    if (user_text === "/text_input") {
-      //respond with "you didn't send me any text"
-      telegram.sendMessage(message.from.id, "you didn't send me any text", {
-        parse_mode: "Markdown",
-      });
-      return;
-    } else {
-      //respond with the text the user sent, user_text
-      telegram.sendMessage(message.from.id, "you sent: " + user_text, {
-        parse_mode: "Markdown",
-      });
-      return;
+    var user_text = message.text.split("/addstress ");
+    console.log(user_text);
+    telegram.sendMessage(user_text);
+    if (user_text === "/addstress") {
+        // telegram.sendMessage(message.from.id, "What new task do you have to stress about?");
+    //   message.text
+
+        }
+        return;
     }
-  }
+
+  telegram.onText(/\/commands/, (msg) => {
+    telegram.sendMessage(
+      msg.chat.id,
+      "These are the commands that are currently available.",
+      {
+        reply_markup: {
+          keyboard: [["/start"], ["/pmme"]],
+          one_time_keyboard: true,
+        },
+      }
+    );
+  });
+
   //return a message to the user when some_state has been changed
   if (user_array[auth_usr(message.from.id)].some_state === 1) {
     //send this message privately
@@ -89,7 +132,6 @@ telegram.on("text", (message) => {
     });
     return;
   }
-});
 //some other event handlers for you to consider
 
 //respond when user sends anything, not very safe, as you can imagine
